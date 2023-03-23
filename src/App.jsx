d6f81adf542c25b894eb3c5cdeb684a6fecf991d
Aryan <aryan4p130202@gmail.com>
2023-03-23 19:57:51 +0530

    feat: featured content component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Home, Sales, Featured } from './components'
-import { heroapi, popularsales, topratesales } from './data/data'
+import { heroapi, popularsales, topratesales, highlight, sneaker} from './data/data'
 const App = () => {
   return (
     <>
     <main className='flex flex-col gap-16 relative'>
       <Home heroapi={heroapi} />
       <Sales feature={popularsales} ifSalesExist/>
+      <Featured content={highlight} />
       <Sales feature={topratesales} />
-      <Featured />
+      <Featured content={sneaker}/>
     </main>
     </>
   )
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,25 +1,23 @@
-const Featured = () => {
+const Featured = ({ content: { heading, title, text, btn, url, img } }) => {
   return (
     <>
+      <div>
         <div>
-            <div>
-                <h1></h1>
-                <h1></h1>
-                <p></p>
-                <a>
-                    <button 
-                        type="button"
-                        className=""
-                    >
-                    </button>
-                </a>
-            </div>
-            <div>
-                <img></img>
-            </div>
+          <h1>{heading}</h1>
+          <h1>{title}</h1>
+          <p>{text}</p>
+          <a href={url}>
+            <button type="button" className="">
+              {btn}
+            </button>
+          </a>
         </div>
+        <div>
+          <img src={img} alt={`img/${heading}`} className=""></img>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
